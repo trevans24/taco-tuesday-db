e81098b99bffa96085e7d4ed1906492c29424c62
Troy Evans <troy@Troys-MacBook-Pro.local>
2017-06-06 11:19:52 -0600

    adding access allowing for outside api calls

--- server.js
+++ server.js
@@ -5,6 +5,27 @@ const app = express();
 const bodyParser = require('body-parser');
 const db = require('./models/');
 
+// FOR ACCESS FROM OUTSIDE SOURCE
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 // Body Parser for JSON
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
